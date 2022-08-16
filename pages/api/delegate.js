037e49f3cdf54a95b9ff8282ac7d19afd958e87d
arr00 <13561405+arr00@users.noreply.github.com>
2022-08-16 17:52:00 -0400

    v input fix

--- pages/api/delegate.js
+++ pages/api/delegate.js
@@ -2,6 +2,18 @@ import { delegate } from "helpers"; // delegate helper
 
 export default async (req, res) => {
   const transaction = req.body; // Collect transaction object
+  
+  // Some providers format EIP-712 sig differently
+  switch(transaction.v) {
+    case "0x00":
+      vInput = "0x1b";
+      break;
+    case "0x01":
+      vInput = "0x1c";
+      break;
+    default:
+      vInput = transaction.v;
+  }
 
   try {
     // Send delegation
